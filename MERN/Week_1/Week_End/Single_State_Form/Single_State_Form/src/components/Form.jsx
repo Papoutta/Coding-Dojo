b29eba305b374e18eaeef85686ac16b0f7498a26
farouk ghezal <faroukghezal3@gmail.com>
2024-12-02 15:44:58 +0100

    update Single State practice

--- MERN/Week_1/Week_End/Single_State_Form/Single_State_Form/src/components/Form.jsx
+++ MERN/Week_1/Week_End/Single_State_Form/Single_State_Form/src/components/Form.jsx
@@ -2,38 +2,44 @@ import React from 'react'
 import { useState } from 'react'
 
 const Form = () => {
-    const [userFirstname,setUserFirstname] = useState("")
-    const [userLastname,setUserLastname] = useState("")
-    const [userEmail,setUserEmail] = useState("")
-    const [userPass,setUserPass] = useState("")
-    const [userConfirmPass,setUserConfirmPass] = useState("")
-    
+    const [user, setUser] = useState({
+        firstname : "",
+        lastname : "",
+        email : "",
+        password : "",
+        confirmPassword : ""
+    })
+
+    const changeHandler = (e) => {
+        setUser((prevUser)=>({...prevUser,[e.target.name] : e.target.value}))
+    }
+
     return(
         <div className='container col-6 border border-2 border-black rounded p-3 d-flex flex-column align-items-center mt-5 mb-5 gap-3' >
             <form className='col-5 d-flex flex-column gap-4'>
                 <div >
                     <label class="form-label">First Name: </label> 
-                    <input className='form-control input-bold-border' onChange={ (e) => setUserFirstname(e.target.value)} value={userFirstname}/>
+                    <input className='form-control input-bold-border' onChange={ (e) => changeHandler(e)} name='firstname' />
 
                 </div>
                 <div >
                     <label class="form-label">Last Name: </label> 
-                    <input className='form-control input-bold-border' onChange={ (e) => setUserLastname(e.target.value)} value={userLastname}/>
+                    <input className='form-control input-bold-border' onChange={ (e) => changeHandler(e)} name='lastname' />
 
                 </div>
                 <div >
                     <label class="form-label">Email:</label> 
-                    <input className='form-control input-bold-border' type='email' onChange={ (e) => setUserEmail(e.target.value)} value={userEmail}/>
+                    <input className='form-control input-bold-border' type='email' onChange={ (e) => changeHandler(e)} name='email' />
 
                 </div>
                 <div >
                     <label class="form-label">Password: </label>
-                    <input className='form-control input-bold-border' type="password" onChange={ (e) => setUserPass(e.target.value)} value={userPass}/>
+                    <input className='form-control input-bold-border' type="password" onChange={ (e) => changeHandler(e)} name='password' />
 
                 </div>
                 <div >
                     <label class="form-label"> Confirm Password: </label>
-                    <input className='form-control input-bold-border' type="password" onChange={ (e) => setUserConfirmPass(e.target.value)} value={userConfirmPass}/>
+                    <input className='form-control input-bold-border' type="password" onChange={ (e) => changeHandler(e)} name='confirmPassword'/>
 
                 </div>
                 <br />
@@ -42,23 +48,23 @@ const Form = () => {
                 <h1 className='mb-4'>Your Form Data :</h1>
                 <div className="d-flex gap-3 align-items-center">
                     <h4>First Name : </h4>
-                    <h5>{userFirstname}</h5>
+                    <h5>{user.firstname}</h5>
                 </div>
                 <div className="d-flex gap-3 align-items-center">
                     <h4>Last Name : </h4>
-                    <h5>{userLastname}</h5>
+                    <h5>{user.lastname}</h5>
                 </div>
                 <div className="d-flex gap-3 align-items-center">
                     <h4>Email : </h4>
-                    <h5>{userEmail}</h5>
+                    <h5>{user.email}</h5>
                 </div>
                 <div className="d-flex gap-3 align-items-center">
                     <h4>Password : </h4>
-                    <h5>{userPass}</h5>
+                    <h5>{user.password}</h5>
                 </div>
                 <div className="d-flex gap-3 align-items-center">
                     <h4>Confirm Password : </h4>
-                    <h5>{userConfirmPass}</h5>
+                    <h5>{user.confirmPassword}</h5>
                 </div>
             </div>
         </div>
